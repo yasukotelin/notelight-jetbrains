ffc74cf2bde6187860009fd39ede2ee21b28124b
GitHub Action <action@github.com>
2020-09-06 13:03:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yasukotelin/notelightjetbrains/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yasukotelin.notelightjetbrains.services
+
+import com.github.yasukotelin.notelightjetbrains.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/yasukotelin/notelightjetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yasukotelin/notelightjetbrains/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yasukotelin.notelightjetbrains.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yasukotelin.notelightjetbrains.MyBundle
 
 class MyProjectService(project: Project) {
 
